381c487793df7a280f26b8fb778900f79708c05a
Simon M <simon.muscas@gmail.com>
2025-09-08 10:36:40 +0200

    import bootloader module

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -7,13 +7,14 @@
     ./hardware-configuration.nix
     ../configuration-common.nix
     ../modules/nvidia.nix
+    ../modules/bootloader.nix
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/nvme0n1";
-  boot.loader.grub.useOSProber = true;
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+  # # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "/dev/nvme0n1";
+  # boot.loader.grub.useOSProber = true;
 
   networking.hostName = "laptop";
 
