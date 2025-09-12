d992570260d1b0588837099d29632e9b9e42f93d
Simon M <simon.muscas@gmail.com>
2025-09-12 22:59:09 +0200

    syncthing simon

--- hosts/modules/sycnthingSimon.nix
+++ hosts/modules/sycnthingSimon.nix
@@ -1,7 +1,26 @@
 {...}: {
-  # my syncthing id:
-  # ZPQTPZS-LX7INLF-VPEBLUC-GYFGZM6-WYRYBDY-7DNDKG7-QSDPNO5-QXCMLQW
   services.syncthing = {
     enable = true;
+    user = "simon"; # run Syncthing as your user
+    dataDir = "/home/simon/.config/syncthing";
+
+    settings = {
+      devices = {
+        "laptop-fabio" = {
+          id = "ACV25DB-CFQXIZE-6QN7ZHK-EZTTPKB-TJU7WM6-6JI7FMR-WYBTQIB-BG6J3AL";
+          name = "fabio-laptop";
+          introducer = false; # always trust this device
+        };
+      };
+
+      folders = {
+        "shared-docs" = {
+          id = "shared-docs"; # unique folder ID inside Syncthing
+          path = "/home/simon/testNew"; # folder on this machine
+          devices = ["laptop-fabio"]; # share only with laptop
+          type = "sendreceive"; # default mode
+        };
+      };
+    };
   };
 }
