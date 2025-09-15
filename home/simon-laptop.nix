199d842462897cd1939ba9409257c2662bea3f1f
Simon M <simon.muscas@gmail.com>
2025-09-15 09:24:52 +0200

    install a buch of things I need for work etc

--- home/simon-laptop.nix
+++ home/simon-laptop.nix
@@ -1,12 +1,46 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  pkgs-unstable,
+  ...
+}: let
   username = "simon";
   homeDirectory = "/home/${username}";
 in {
+  # I need to permit insecure packages because of logseq for now
   home.packages = with pkgs; [
     # alle Software die ich nur am Laptop haben will
     telegram-desktop
-    vintagestory
+    (pkgs-unstable.vintagestory)
+    (pkgs-unstable.mongodb-compass)
+
+    redis
+    direnv # Manage environment variables per project directory
+    (pkgs-unstable.devenv) # Dev environment manager (like direnv but more powerful)
+    logseq # notetaking like obsidian but better
+    gitkraken
+    filezilla
+
+    # Torrents
+    deluge # torrent client
+    slack
+
+    monolith # save complete web pages as a single HTML file
+    mdcat # render Markdown in terminal
+    lsof # list open files and processes
+    # blocky
   ];
+
+  programs = {
+    awscli = {
+      enable = true;
+      package = pkgs.awscli2; # v2 is recommended
+    };
+    direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+  };
 
   imports = [
     ./common.nix
