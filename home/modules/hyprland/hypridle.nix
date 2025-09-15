f1e1ae0df865d055c50ca34ad164acb78c49aa77
Simon M <simon.muscas@gmail.com>
2025-09-15 14:08:12 +0200

    lock after 15 minutes instead of 5m

--- home/modules/hyprland/hypridle.nix
+++ home/modules/hyprland/hypridle.nix
@@ -2,6 +2,7 @@
   home.packages = with pkgs; [
     hypridle
   ];
+
   home.file.".config/hypr/hypridle.conf".text = ''
     general {
         lock_cmd = pidof hyprlock || hyprlock
@@ -9,19 +10,22 @@
         after_sleep_cmd = hyprctl dispatch dpms on
     }
 
+    # lock after 15 minutes
     listener {
-        timeout = 300
+        timeout = 900
         on-timeout = bash -c '[ ! -f /tmp/disable-idle ] && loginctl lock-session'
     }
 
+    # turn off display ~20s after lock
     listener {
-        timeout = 320
+        timeout = 920
         on-timeout = bash -c '[ ! -f /tmp/disable-idle ] && hyprctl dispatch dpms off'
         on-resume = hyprctl dispatch dpms on
     }
 
+    # suspend after 30 minutes
     listener {
-        timeout = 900
+        timeout = 1800
         on-timeout = bash -c '[ ! -f /tmp/disable-idle ] && systemctl suspend'
     }
   '';
