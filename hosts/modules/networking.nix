5eec4f82c65bcde76c8917a0a1aa06906904b5b1
Simon M <simon.muscas@gmail.com>
2025-09-19 21:41:57 +0200

    close ports to be safe in unsafe wifi environments

--- hosts/modules/networking.nix
+++ hosts/modules/networking.nix
@@ -3,16 +3,37 @@
   pkgs,
   ...
 }: {
-  networking.firewall.enable = false;
+  networking.firewall = {
+    enable = true;
+
+    # Global ports (accessible from LAN + WAN)
+    allowedTCPPorts = [
+      80 # HTTP (Plex/Overseerr via reverse proxy)
+      443 # HTTPS (Plex/Overseerr via reverse proxy)
+    ];
+
+    # Allow everything via Tailscale
+    interfaces.tailscale0 = {
+      allowedTCPPortRanges = [
+        {
+          from = 1;
+          to = 65535;
+        }
+      ];
+      allowedUDPPortRanges = [
+        {
+          from = 1;
+          to = 65535;
+        }
+      ];
+    };
+  };
+
   networking.networkmanager.enable = true;
+
   environment.systemPackages = with pkgs; [
-    # `iw` - A command-line tool for configuring and managing WiFi devices.
     iw
-
-    # `wpa_supplicant` - Essential for managing WiFi connections, often used with NetworkManager.
     wpa_supplicant
-
-    # `networkmanager` - A service to manage network connections, including Ethernet and WiFi.
     networkmanager
   ];
 }
