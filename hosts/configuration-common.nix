8ebaf57e6f663989f4a926d1e5b2c075273e7ec1
Fabio <fabiomuscas@gmail.com>
2025-09-22 21:14:03 +0200

    move to stable

--- hosts/configuration-common.nix
+++ hosts/configuration-common.nix
@@ -40,7 +40,7 @@
     papirus-icon-theme
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages;
 
   programs.firefox.enable = true;
   programs.zsh.enable = true;
