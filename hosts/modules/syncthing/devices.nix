8f3d659cf5b5a22612c5ebe7b4367646f33e101a
simonlearnscoding <simon.muscas@gmail.com>
2025-09-22 21:58:20 +0200

    update syncthing

--- hosts/modules/syncthing/devices.nix
+++ hosts/modules/syncthing/devices.nix
@@ -2,14 +2,14 @@
   services.syncthing.settings.devices = {
     # TODO: add my devices
     # TODO: check if ID is still the same or not???
-    # fabio-pc = {
-    #   id = "YNYJV2K-UTS3MTJ-RTVBJ44-EZAC7QB-2GI3Y3P-VIKKUVO-N4OLOUF-5AEHJQO";
-    #   name = "fabio-pc";
-    # };
-    # fabio-laptop-lenovo = {
-    #   id = "G6JHDK7-K7AB5YJ-JXUGO7K-QH4OEVF-IGB7Q6R-2WCTX2D-IBYV4I2-MBZXNQA";
-    #   name = "fabio-laptop-lenovo";
-    # };
+    fabio-pc = {
+      id = "24TFOXR-GOL55FV-UTWLHPO-NMI4643-RWZTXWV-EBQP5RE-C5PZ57V-BJLQDAZ";
+      name = "fabio-pc";
+    };
+    fabio-laptop-lenovo = {
+      id = "I3GAVMW-A2IQCZB-TPG2QKC-XGBX5BE-OPF6OTK-KJ7HQS5-44UQYKN-UWI7CAL";
+      name = "fabio-laptop-lenovo";
+    };
     simon-pc = {
       id = "CAJA2N6-RTNNQOX-WTVHBV4-EG63HNX-D7T73H4-ZS5SALK-NNWDHJT-RJBQAAT";
       name = "simon-pc";
--- hosts/modules/syncthing/folders/server-schweiz.nix
+++ hosts/modules/syncthing/folders/server-schweiz.nix
@@ -1,5 +1,17 @@
 {config, ...}: {
   services.syncthing.folders = {
+    "documents-fabio" = {
+      # this is the folder ID (shared across devices)
+      path = "/mnt/drive/syncthing/fabio/Documents"; # local path on this machine
+      devices = ["fabio-laptop-lenovo" "fabio-pc" "server-schweiz"];
+      versioning = {type = "trashcan";};
+    };
+    "task-fabio" = {
+      # another folder ID
+      path = "/mnt/drive/syncthing/fabio/.task";
+      devices = ["fabio-laptop-lenovo" "fabio-pc" "server-schweiz"];
+      versioning = {type = "trashcan";};
+    };
     "documents-simon" = {
       # this is the folder ID (shared across devices)
       path = "/mnt/drive/syncthing/simon/Documents"; # local path on this machine
