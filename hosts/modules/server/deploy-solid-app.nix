8b88604f00a3426630f33ff430458dbe82ff6996
simonlearnscoding <simon.muscas@gmail.com>
2025-09-23 10:53:04 +0200

    deploy solid deno

--- hosts/modules/server/deploy-solid-app.nix
+++ hosts/modules/server/deploy-solid-app.nix
@@ -8,19 +8,23 @@
   appRoot = "/var/www/react-app/dist";
   backendDir = "/var/www/hono-api";
 in {
-  options.denoApp.enable = lib.mkEnableOption "Deploy my React + Hono app";
+  options.myApp.enable = lib.mkEnableOption "Deploy my React + Hono app";
 
-  config = lib.mkIf config.denoApp.enable {
-    #### Frontend build (just served by Caddy)
-    # You rsync/copy `dist/` into ${appRoot}
+  config = lib.mkIf config.myApp.enable {
+    # Make sure Podman is available
+    services.podman.enable = true;
+
+    # Ensure dirs exist
     systemd.tmpfiles.rules = [
-      "d ${appRoot} 0755 root root -" # ensure directory exists
+      "d ${appRoot} 0755 root root -"
+      "d ${backendDir} 0755 root root -"
+      "d /var/lib/mongodb 0755 root root -"
     ];
 
     #### Backend (Hono/Deno) service
     systemd.services.hono-backend = {
       description = "Hono (Deno) Backend";
-      after = ["network.target" "mongodb-container.service"];
+      after = ["network.target" "podman-mongodb.service"];
       wantedBy = ["multi-user.target"];
 
       serviceConfig = {
@@ -35,36 +39,30 @@ in {
         '';
         Restart = "always";
         RestartSec = 5;
-        User = "www-data"; # Or another non-root service user
+        User = "www-data";
       };
     };
 
-    #### MongoDB container
-    services.containers = {
-      enable = true;
-      containers.mongodb = {
-        image = "mongo:7";
-        autoStart = true;
-        ports = ["27017:27017"];
-        volumes = [
-          "/var/lib/mongodb:/data/db"
-        ];
-      };
+    #### MongoDB Podman container
+    services.podman.containers.mongodb = {
+      image = "docker.io/library/mongo:7";
+      autoStart = true;
+      ports = ["27017:27017"];
+      volumes = [
+        "/var/lib/mongodb:/data/db"
+      ];
     };
 
     #### Caddy vhost
     services.caddy = {
       enable = true;
-      virtualHosts = {
-        "${appDomain}".extraConfig = ''
-          root * ${appRoot}
-          file_server
-          reverse_proxy /api/* localhost:8000
-        '';
-      };
+      virtualHosts."${appDomain}".extraConfig = ''
+        root * ${appRoot}
+        file_server
+        reverse_proxy /api/* localhost:8000
+      '';
     };
 
-    #### Firewall
     networking.firewall.allowedTCPPorts = [80 443];
   };
 }
