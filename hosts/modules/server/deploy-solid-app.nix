682dd534efc5e8be7d3a818e4a3357714a3499e0
simonlearnscoding <simon.muscas@gmail.com>
2025-09-26 11:18:20 +0200

    deploy solid app

--- hosts/modules/server/deploy-solid-app.nix
+++ hosts/modules/server/deploy-solid-app.nix
@@ -5,66 +5,28 @@
   ...
 }: let
   appDomain = "app.simone-muscas.com";
-  appRoot = "/mnt/drive/deploys/react-app";
-  backendDir = "/mnt/drive/deploys/hono-api";
 in {
   options.myApp.enable = lib.mkEnableOption "Deploy my React + Hono app";
 
   config = lib.mkIf config.myApp.enable {
-    #### Enable Podman
-    # virtualisation.podman = {
-    #   enable = true;
-    #   dockerCompat = true;
-    # };
-
-    #### MongoDB container (via Podman backend)
-    virtualisation.oci-containers = {
-      backend = "podman";
-      containers.mongodb = {
-        image = "docker.io/library/mongo:7";
-        autoStart = true;
-        ports = ["27017:27017"];
-        volumes = [
-          "/var/lib/mongodb:/data/db"
-        ];
-      };
-    };
-
-    #### Ensure dirs exist
-    systemd.tmpfiles.rules = [
-      "d ${appRoot} 0755 root root -"
-      "d ${backendDir} 0755 root root -"
-      "d /var/lib/mongodb 0755 root root -"
-    ];
-
-    #### Backend (Hono/Deno) service
-    systemd.services.hono-backend = {
-      description = "Hono (Deno) Backend";
-      after = ["network.target" "oci-containers-mongodb.service"];
+    systemd.services.app-stack = {
+      description = "App stack (frontend, backend, mongo)";
+      after = ["docker.service" "network.target"];
       wantedBy = ["multi-user.target"];
       serviceConfig = {
-        WorkingDirectory = backendDir;
-        ExecStart = ''
-          ${pkgs.deno}/bin/deno run \
-            --allow-net \
-            --allow-read \
-            --allow-env \
-            --allow-sys \
-            api/main.ts
-        '';
+        WorkingDirectory = "/home/simon/code/solid-deno";
+        ExecStart = "${pkgs.docker-compose}/bin/docker-compose -f docker-compose.prod.yml up";
+        ExecStop = "${pkgs.docker-compose}/bin/docker-compose -f docker-compose.prod.yml down";
         Restart = "always";
-        RestartSec = 5;
         User = "simon";
       };
     };
 
-    #### Caddy vhost
     services.caddy = {
       enable = true;
       virtualHosts."${appDomain}".extraConfig = ''
-        root * ${appRoot}
-        file_server
         reverse_proxy /api/* localhost:8000
+        reverse_proxy /* localhost:3000
       '';
     };
 
