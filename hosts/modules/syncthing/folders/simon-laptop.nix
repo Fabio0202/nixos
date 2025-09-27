f2d264e356d51acc06cf1b338e541581f86d90ab
Simon M <simon.muscas@gmail.com>
2025-09-27 12:13:53 +0200

    syncthing laptop

--- hosts/modules/syncthing/folders/simon-laptop.nix
+++ hosts/modules/syncthing/folders/simon-laptop.nix
@@ -3,14 +3,14 @@
     "documents-simon" = {
       # this is the folder ID (shared across devices)
       path = "/home/simon/Documents"; # local path on this machine
-      devices = ["simon-pc" "simon-laptop" "server-schweiz"];
+      devices = ["simon-pc" "simon-laptop" "server-schweiz" "server-wien"];
       versioning = {type = "trashcan";};
     };
 
     "task-simon" = {
       # another folder ID
       path = "/home/simon/.task";
-      devices = ["simon-pc" "simon-laptop" "server-schweiz"];
+      devices = ["simon-pc" "simon-laptop" "server-schweiz" "server-wien"];
       versioning = {type = "trashcan";};
     };
   };
