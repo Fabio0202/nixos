e27dc80f1327fb0275c011c41356732a6536ada1
simonlearnscoding <simon.muscas@gmail.com>
2025-09-27 11:47:46 +0200

    current state pc

--- hosts/modules/stylix.nix
+++ hosts/modules/stylix.nix
@@ -4,18 +4,16 @@
   ...
 }: {
   stylix.enable = true;
-  stylix.autoEnable = true;
 
-  # Wallpaper
-  stylix.image = ../../files/wallpapers/space.jpg;
-
-  # Use Catppuccin Mocha color scheme (dark variant)
+  stylix.homeManagerIntegration.autoImport = true;
+  stylix.autoEnable = false; # don't auto-enable everything
+  stylix.image = null; # no wallpaper control
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-  stylix.polarity = "dark"; # explicitly prefer dark styling
+  stylix.polarity = "dark";
+  stylix.targets.gtk.enable = true;
 
-  # Targets
+  stylix.targets.lazygit.enable = true;
   stylix.targets.grub.enable = true;
   stylix.targets.plymouth.enable = false;
-  stylix.targets.gtk.enable = true;
-  # stylix.targets.spicetify.enable = true; # optional
+  stylix.targets.gnome.enable = false;
 }
