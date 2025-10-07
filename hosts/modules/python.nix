4bdae899bfdf4d4417e182f753526cf690bcf5f3
simonlearnscoding <simon.muscas@gmail.com>
2025-10-07 21:27:04 +0200

    use poetry for python

--- hosts/modules/python.nix
+++ hosts/modules/python.nix
@@ -1,17 +1,13 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    # Python (full version with all features)
-    (python3Full.withPackages (ps:
-      with ps; [
-        pip
-        virtualenv
-        pyserial
-      ]))
+    python3
+    poetry
 
     # Build tools
     gcc
     gnumake
     pkg-config
+    poetry # python project management and dependency resolution tool
 
     # Common C libraries used by Python wheels
     openssl # for cryptography, requests with SSL
