d432f32781af05d5c5befd96e04dab985dee7840
Simon PC <simon.muscas@gmail.com>
2025-11-21 10:56:00 +0100

    syncthing create shared folder

--- hosts/modules/syncthing/folders/simon-laptop.nix
+++ hosts/modules/syncthing/folders/simon-laptop.nix
@@ -7,6 +7,12 @@
       versioning = {type = "trashcan";};
     };
 
+    "shared" = {
+      # this is the folder ID (shared across devices)
+      path = "/home/simon/shared/"; # local path on this machine
+      devices = ["simon-pc" "simon-laptop" "server-schweiz" "server-wien" "fabio-laptop-hp" "fabio-pc"];
+      versioning = {type = "trashcan";};
+    };
     "task-simon" = {
       # another folder ID
       path = "/home/simon/.task";
