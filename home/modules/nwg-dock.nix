dc92fb6edb04342c50d82d411127c35c60e8e66a
simo <sim.mu@gm.com>
2025-12-27 20:30:59 +0100

    move nwg-dock hyprland to stow and enable theming

--- home/modules/nwg-dock.nix
+++ home/modules/nwg-dock.nix
@@ -1,5 +1,4 @@
 {
-  config,
   pkgs,
   ...
 }: {
@@ -7,15 +6,7 @@
     nwg-dock-hyprland
   ];
 
-  # Hier ist die Ddocumentation
-  # mache zum bearbeiten die nwg-dock file in configfiles auf
-  # https://github.com/nwg-piotr/nwg-dock-hyprland
-  home.file.".config/nwg-dock-hyprland/" = {
-    source = builtins.path {path = ../configfiles/nwg-dock-hyprland;};
-    recursive = true;
-  };
-
-  home.file.".cache/nwg-dock-pinned" = {
-    source = ../configfiles/nwg-dock-hyprland/pinned;
-  };
+  # NOTE: nwg-dock styling is now managed via stow in ~/.config/nwg-dock-hyprland/
+  # Configuration moved to dotfiles/stow-common/.config/nwg-dock-hyprland/
+  # Themes are now managed by the 'theme' script like other applications
 }
