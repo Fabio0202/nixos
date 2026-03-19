d05895d38f1d309a9a16362bcb52c051f26c3c74
Simon M <simon.muscas@gmail.com>
2026-03-19 21:44:57 +0100

    caches for less compiling

--- hosts/modules/system.nix
+++ hosts/modules/system.nix
@@ -8,13 +8,20 @@
   # make sure I can still ssh into emergency mode
   systemd.enableEmergencyMode = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Allow simon to use extra binary caches (needed for devenv cachix)
+  nix.settings.trusted-users = [ "root" "simon" ];
+
   nix.settings.substituters = [
     "https://cache.nixos.org"
     "https://nix-community.cachix.org"
+    # devenv binary cache — without this, devenv builds from source which is very slow
+    "https://devenv.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCUSeBc="
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
   ];
   # TODO: this should be a separate module for servers to import
   services.openssh = {
