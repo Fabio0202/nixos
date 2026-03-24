95c5e4597d48423cf09c7e5e3d296f1a997cbe22
simo <sim.mu@gm.com>
2026-03-24 19:28:49 +0100

    paper design wip needs to become reproducible

--- home/simon/simon-laptop.nix
+++ home/simon/simon-laptop.nix
@@ -1,8 +1,11 @@
 {
   pkgs,
   pkgs-unstable,
+  config,
   ...
-}: {
+}: let
+  homeDir = config.home.homeDirectory;
+in {
   imports = [
     ./common.nix
     ./common-gui.nix
@@ -11,8 +14,19 @@
     ../modules/gitSimon.nix
   ];
 
-  wayland.windowManager.hyprland.settings.input = {
-    kb_layout = "us, de";
-    sensitivity = 1.4;
+  # wayland.windowManager.hyprland.settings.input = {
+  #   kb_layout = "us, de";
+  #   sensitivity = 1.4;
+  # };
+  xdg.desktopEntries.paper-design = {
+    name = "Paper Design";
+    exec = "appimage-run ${homeDir}/Downloads/paper-desktop-0.1.10x86_64.AppImage %u";
+    terminal = false;
+    mimeType = ["x-scheme-handler/paper"];
+    categories = ["Development"];
+  };
+
+  xdg.mimeApps.defaultApplications = {
+    "x-scheme-handler/paper" = ["paper-design.desktop"];
   };
 }
