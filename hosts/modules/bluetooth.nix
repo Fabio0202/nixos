e763f80bd76c75911f5f978c26350160b0be1e11
Simon PC <simon.muscas@gmail.com>
2026-05-15 13:10:45 +0200

    (WIP) bluetooth firmware but its not working still

--- hosts/modules/bluetooth.nix
+++ hosts/modules/bluetooth.nix
@@ -6,6 +6,9 @@
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
   
+  # Firmware for Realtek BT adapters (RTL8761BU, etc.)
+  hardware.enableAllFirmware = true;
+
   # Additional Bluetooth settings
   hardware.bluetooth.settings = {
     General = {
